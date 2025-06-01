020f4cd297589d2b3314f8551e35c0f492686ea7
Vilizar Denichin <denichinv@gmail.com>
2025-06-01 17:18:39 +0100

    css edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,49 @@
-import './App.css'
-import React from 'react';
-import ProfileCard from './ProfileCard';
+import React from "react";
+import ProfileCard from "./ProfileCard";
 
 const App = () => {
   const profiles = [
     {
       image:
-        'https://cdn.continental.com/fileadmin/__imported/sites/corporate/_international/german/hubpages/10_20presse/04_mediathek/vorstand_20_28de_29/philip-nelles.jpg',
-      name: 'John Doe',
-      jobTitle: 'Frontend Developer',
-      bio: 'Passionate about creating user-friendly web applications.',
+        "https://cdn.continental.com/fileadmin/__imported/sites/corporate/_international/german/hubpages/10_20presse/04_mediathek/vorstand_20_28de_29/philip-nelles.jpg",
+      name: "John Doe",
+      jobTitle: "Frontend Developer",
+      bio: "Passionate about creating user-friendly web applications.",
     },
 
     {
       image:
-        'https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture.jpg',
-      name: 'Jane Smith',
-      jobTitle: 'UX Designer',
-      bio: 'Creating intuitive and engaging user experiences.',
+        "https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture.jpg",
+      name: "Jane Smith",
+      jobTitle: "UX Designer",
+      bio: "Creating intuitive and engaging user experiences.",
     },
     {
       image:
-        'https://st2.depositphotos.com/1715570/5435/i/450/depositphotos_54357355-stock-photo-handsome-young-black-man-smiling.jpg',
-      name: 'Mike Johnson',
-      jobTitle: 'Backend Engineer',
-      bio: 'Building robust and scalable server-side solutions.',
+        "https://st2.depositphotos.com/1715570/5435/i/450/depositphotos_54357355-stock-photo-handsome-young-black-man-smiling.jpg",
+      name: "Mike Johnson",
+      jobTitle: "Backend Engineer",
+      bio: "Building robust and scalable server-side solutions.",
     },
   ];
-  return (<>
-      <h1 className='header'>Team Profiles</h1><hr />
-    <div className="app">
-      <div className="profiles-container">
-        {profiles.map((profile, index) => (
-          <ProfileCard
-            key={index}
-            image={profile.image}
-            name={profile.name}
-            jobTitle={profile.jobTitle}
-            bio={profile.bio}
-          />
-        ))}
+  return (
+    <>
+      <h1 className="header">Team Profiles</h1>
+      <hr />
+      <div className="app">
+        <div className="profiles-container">
+          {profiles.map((profile, index) => (
+            <ProfileCard
+              key={index}
+              image={profile.image}
+              name={profile.name}
+              jobTitle={profile.jobTitle}
+              bio={profile.bio}
+            />
+          ))}
+        </div>
       </div>
-    </div></>
+    </>
   );
 };
 
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './ProfileCard.css'
+import React from "react";
+import "./ProfileCard.css";
 
 const ProfileCard = ({ image, name, jobTitle, bio }) => {
   return (
     <div className="profile-card">
       <img src={image} alt={`${name}'s profile`} className="profile-image" />
       <div className="profile-details">
-        <h2>{name}</h2><hr />
+        <h2>{name}</h2>
+        <hr />
         <h3>{jobTitle}</h3>
         <p>{bio}</p>
       </div>
