226bcaee24dda6cce9e0d4e904b81ebbddab01dd
Vilizar Denichin <denichinv@gmail.com>
2025-06-05 03:13:49 +0100

    refactor: moved profile data to separate profiles.js file

---
+++ src/profiles.js
@@ -0,0 +1,30 @@
+export const profiles = [
+  {
+    image:
+      "https://cdn.continental.com/fileadmin/__imported/sites/corporate/_international/german/hubpages/10_20presse/04_mediathek/vorstand_20_28de_29/philip-nelles.jpg",
+    name: "John Doe",
+    jobTitle: "Frontend Developer",
+    bio: "Passionate about creating user-friendly web applications.",
+    linkedin: "https://www.linkedin.com/in/johndoe",
+    github: "https://www.github.com/johndoe",
+  },
+
+  {
+    image:
+      "https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture.jpg",
+    name: "Jane Smith",
+    jobTitle: "UX Designer",
+    bio: "Creating intuitive and engaging user experiences.",
+    linkedin: "https://www.linkedin.com/in/janesmith",
+    github: "https://www.github.com/janesmith",
+  },
+  {
+    image:
+      "https://st2.depositphotos.com/1715570/5435/i/450/depositphotos_54357355-stock-photo-handsome-young-black-man-smiling.jpg",
+    name: "Mike Johnson",
+    jobTitle: "Backend Engineer",
+    bio: "Building robust and scalable server-side solutions.",
+    linkedin: "https://www.linkedin.com/in/mikejohnson",
+    github: "https://www.github.com/mikejohnson",
+  },
+];
